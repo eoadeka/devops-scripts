dd85e55174706dffe56923296b1f50df4e83d353
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-02-05 00:52:59 +0000

    script: Clone Flask github repo
    Clone Flask application from Github repository

---
+++ aws/clone_flask_github_repo_on_ec2_linux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+    sleep 1
+done   
+
+# Update all packages
+sudo yum update -y
+
+# Install new packages
+sudo yum install -y httpd git python3 python3-pip python3-devel virtualenv
+
+# Verify python installation by checking version
+sudo python3 --version
+
+# Enable and start Apache (httpd)
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+# Clone Github repository
+# Replace <github-repo-link> with your repo name #
+sudo git clone <github-repo-link> /var/www/html/web/
+
+# Navigate to app directory
+sudo cd /var/www/html/web/my_app/
+
+# Create virtual environment and install dependencies
+sudo python3 -m venv venv
+source venv/bin/activate
+sudo python3 -m pip install -r requirements.txt
+
+# Export flask app environment variable
+export FLASK_APP = app.py
+
+# Run the Flask app
+flask run --host=0.0.0.0 --port=5000
